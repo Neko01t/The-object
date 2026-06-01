f3e089ff0f0649be2f94ad0069aaacb04914f6a8
neko01t <omsonawane124421@gmail.com>
2026-06-01 18:20:19 +0530

    Project init

---
+++ src/components/Hero.jsx
@@ -0,0 +1,5 @@
+export default function Hero() {
+  return (<div className="w-full bg-background text-foreground flex justify-center items-center">
+    <div className="text-2xl font-warning" >Love</div>
+  </div>)
+}
