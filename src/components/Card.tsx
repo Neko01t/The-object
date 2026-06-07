d83bfe80541513499c66f7c7165b58aad1e98bc3
neko01t <omsonawane124421@gmail.com>
2026-06-07 20:27:17 +0530

    2nd object added
    - Added the assests for 2nd object

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,18 +9,6 @@ interface CardProps {
 
 export default function Card({ info }: CardProps) {
   const [activeView, setActiveView] = useState('default');
-
-  // const info = {
-  //   title: "Blender Donut",
-  //   description: "A classic 3D donut model. Includes icing, sprinkles, and a baked texture.",
-  //   verts: 45893,
-  //   faces: 45853,
-  //   isRigged: "No",
-  //   imageRender: "/models/donut/render.png",
-  //   glbPath: "/models/donut/view.glb",
-  //   blendPath: "/models/donut/donut.blend"
-  // };
-  //
   return (
     <div className="max-w-sm rounded-xl overflow-hidden bg-card text-card-foreground shadow-lg border border-border relative">
 
